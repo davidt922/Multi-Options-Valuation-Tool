3e4aaeac035f1581e317184ac8dbfeb97fbad3d1
David Teixé Molins <david.teixe@cepsa.com>
2021-04-11 13:48:25 +0200

    Add comments to meanPayout function

--- MeanPayoutTest.m
+++ MeanPayoutTest.m
@@ -1,7 +1,10 @@
 exerciceFunction = @(actualDate, maturityDate, stepSize)   true(size(actualDate));
+
 barrier = @(subyacentPrice) true(size(subyacentPrice)).*-1;
+
 payoff = @(subyacentPrice, actualDate, maturityDate) max(subyacentPrice - 10, 0);
-path = [10, 15, 12, 17];
+
+path = [10, 15, 9, 17];
 stepDatetimeArray = [datetime(2021, 04, 10), datetime(2021, 04, 11), datetime(2021, 04, 12), datetime(2021, 04, 13)];
 interestRateArray = [0,0,0,0];
 maturity = datetime(2021, 04, 13);
